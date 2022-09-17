3281e1506759c53ca886cca07d02b5e113564268
KseniaOts <113545330+KseniaOts@users.noreply.github.com>
2022-09-17 15:28:21 +0300

    Deleted current button

--- src/index.js
+++ src/index.js
@@ -44,18 +44,4 @@ function handleSubmit(event) {
   searchCity(cityName);
 }
 
-function searchLocation(position) {
-  let apiKey = "1be85983e26932dcdad5b28a3c894628";
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(displayWeatherCondition);
-}
-
-function getCurrentLocation(event) {
-  event.preventDefault();
-  navigator.geolocation.getCurrentPosition(searchLocation);
-}
-
-let currentLocationButton = document.querySelector("#current-location-button");
-currentLocationButton.addEventListener("click", getCurrentLocation);
-
 searchCity("London");
